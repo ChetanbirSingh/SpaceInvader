c41c6cbc377539b913b47dfea279fa02f03a4dda
Chetanbir Singh <chetanbirr@gmail.com>
2025-08-16 01:20:48 -0600

    Add Basic Collision

--- Entities/enemy.py
+++ Entities/enemy.py
@@ -4,9 +4,14 @@ from Entities.entity import Entity
 class Enemy(Entity):
     def __init__(self, img_path, screen_width, screen_height, speed=0.5):
         # Random initial position
-        x = random.randint(0, screen_width - 64)
+        x = random.randint(0, screen_width)
         y = random.randint(50, 150)
+
         super().__init__(img_path, x, y, screen_width, screen_height, speed)
+
+    def re_spawn(self):
+        self.x = random.randint(0, self.screen_width - 64)
+        self.y = random.randint(50, 150)
 
     def update_position(self):
         # Move horizontally
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pygame
+import math
 
-from Entities.bullet import Bullet
 from Entities.player import Player
 from Entities.enemy import Enemy
 
@@ -8,6 +8,8 @@ pygame.init()
 
 screen_width = 1000
 screen_height = 700
+
+player_score = 0
 
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("Space Invaders")
@@ -21,6 +23,11 @@ player = Player(img_path='Assets/Img/player.png', x=370, y=580, screen_width=scr
 
 # Create enemy instance
 enemy = Enemy(img_path='Assets/Img/Enemies/enemy.png', screen_width=screen_width, screen_height=screen_height)
+
+
+def is_collision(player, enemy):
+    distance = math.sqrt(math.pow((bullet.x - enemy.x), 2) + math.pow(bullet.y - enemy.y, 2))
+    return distance < 27
 
 running = True
 while running:
@@ -43,6 +50,15 @@ while running:
     player.update_bullets()
     player.display(screen)
     player.display_bullets(screen)
+
+    # Collision
+    for bullet in player.bullets[:]:  # iterate over a copy to safely remove
+        bullet.update_position()
+        if is_collision(bullet, enemy):
+            player.bullets.remove(bullet)
+            player_score += 1
+            print(player_score)
+            enemy.re_spawn()
 
     enemy.display(screen)      # Draw the enemy on the screen
     enemy.update_position()    # Move the enemy
