fb79bb15fc9f0fc8c37e3c70a7ed6e5ea13dfa10
Chetanbir Singh <chetanbirr@gmail.com>
2025-08-15 18:57:29 -0600

    Change Title Icon, background

--- main.py
+++ main.py
@@ -4,6 +4,11 @@ pygame.init() # Initialize pygame
 # Create the screen (height,width)
 screen = pygame.display.set_mode((800, 600))
 
+# Title and Icon
+pygame.display.set_caption("Space Invaders")
+icon = pygame.image.load('img/logo.png')
+pygame.display.set_icon(icon)
+
 # Game Loop
 running = True
 while running:
@@ -11,3 +16,9 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+    # Change background color
+    screen.fill((25, 23, 25))
+
+    # Update Screen
+    pygame.display.update()
